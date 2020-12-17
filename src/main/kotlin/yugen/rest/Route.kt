e115a22d52f375d25eb0bb23a02aec5e4c21031d
Lewis Crichton <lewistehminerz@outlook.com>
2020-12-17 20:06:03 +0000

    move routes into internal class

--- src/main/kotlin/yugen/rest/Route.kt
+++ src/main/kotlin/yugen/rest/Route.kt
@@ -4,25 +4,41 @@ import okhttp3.Request
 import okhttp3.RequestBody
 import yugen.YugenOptions
 
+/**
+ * Discord API routes.
+ */
 internal object Routes {
     val GetGateway = Route("GET", "/gateway/bot")
-}
 
-internal class Route(private val method: String, private val url: String) {
-    fun toRequest(params: Map<String, String>? = null, body: RequestBody? = null, token: String? = null): Request {
-        val request = Request.Builder().url(
-            YugenOptions.apiBase + run {
-                var formattedUrl = url
-                params?.forEach {
-                    formattedUrl = formattedUrl.replace(":" + it.key, it.value)
+    /**
+     * A route.
+     *
+     * @param method The HTTP method for this route.
+     * @param url The API URL to this route.
+     */
+    internal class Route(private val method: String, private val url: String) {
+        /**
+         * Turns the route into a request.
+         *
+         * @param params The parameters to fill in the URL.
+         * @param body A request body.
+         * @param token The bot token.
+         */
+        fun toRequest(params: Map<String, String>? = null, body: RequestBody? = null, token: String? = null): Request {
+            val request = Request.Builder().url(
+                YugenOptions.apiBase + run {
+                    var formattedUrl = url
+                    params?.forEach {
+                        formattedUrl = formattedUrl.replace(":" + it.key, it.value)
+                    }
+                    formattedUrl
                 }
-                formattedUrl
-            }
-        ).method(method, body)
+            ).method(method, body)
 
-        if (token != null)
-            request.addHeader("Authorization", "Bot $token")
+            if (token != null)
+                request.addHeader("Authorization", "Bot $token")
 
-        return request.build()
+            return request.build()
+        }
     }
 }
