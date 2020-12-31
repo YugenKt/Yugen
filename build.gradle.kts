41bf9ab8981f6122f635b378e9de69a6f11cca22
Lewis Crichton <lewistehminerz@outlook.com>
2020-12-31 15:01:19 +0000

    add parameterized tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 
     // junit
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
