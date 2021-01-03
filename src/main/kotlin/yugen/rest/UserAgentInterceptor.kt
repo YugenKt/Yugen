220d735eb43cde8b6fe6166b6c0697dbe9b74ce2
Lewis Crichton <lewistehminerz@outlook.com>
2021-01-03 15:06:54 +0000

    i was wrong

--- src/main/kotlin/yugen/rest/UserAgentInterceptor.kt
+++ src/main/kotlin/yugen/rest/UserAgentInterceptor.kt
@@ -4,7 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 import yugen.YugenOptions
 
-internal class UserAgentInterceptor: Interceptor {
+internal class UserAgentInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return chain.proceed(
             chain.request().newBuilder().header("User-Agent", YugenOptions.userAgent).build()
